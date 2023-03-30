c681ae72f89e50ae39988eafc95138420b443c3e
Viktor <50486210+vpodhornyi@users.noreply.github.com>
2023-03-30 17:48:43 +0300

    logger

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 import express from "express";
+import fs from "fs";
 
 const PORT = 8080;
 const app = express();
 
+app.use((req, res) => {
+  fs.appendFile('logger.txt', `Address: ${req.ip}`, err => {
+    console.log('log has been wrouten');
+  });
+})
+
 app.get('/', (req, res) => {
+  res.cookie('someCookie', 'test cookie', {
+    httpOnly: true,
+    maxAge: 2000
+  });
+  res.cookie('anotherCookie', 'another cookie');
+
   res.send(`
   <h1>Main page</h1>
   <ul>
@@ -20,6 +33,11 @@ app.get('/about', (req, res) => {
 app.get('/products', (req, res) => {
   res.send("<h1>Products page</h1>");
 });
+
+app.get('/category/:categoryId', (req, res) => {
+  const categoryId = req.params['categoryId'];
+  res.send(`request param is ${categoryId}`);
+})
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 })
